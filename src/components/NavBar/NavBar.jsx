ac02841bf5a940a10676c184e8adc2d662123066
juan sacco <juandsacco@gmail.com>
2023-03-21 02:19:30 -0300

    agrego form en pag contacto y boton desplegable en navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,17 @@ const NavBar = () => {
         <div className='containerNavBar'>
             <ul className='ulNavBar'>
                 <Link to={'/'}><li className='liNavBar'>Inicio</li></Link>
-                <li className='liNavBar'>Recomendaciones</li>
+                <div class="btn-group" role="group">
+                    <button id='buttonRecomendaciones' type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Recomendaciones</button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Alimentación</a>
+                        <a class="dropdown-item" href="#">Actividad física</a>
+                        <a class="dropdown-item" href="#">Prevención cardiovascular</a>
+                        <a class="dropdown-item" href="#">Prevención oftalmológica</a>
+                        <a class="dropdown-item" href="#">Pie diabético</a>
+                        <a class="dropdown-item" href="#">Tratamientos</a>
+                    </div>
+                </div>
                 <Link to={'/actividades'}><li className='liNavBar'>Actividades</li></Link>
                 <Link to={'/quienes-somos'}><li className='liNavBar'>Quiénes somos</li></Link>
                 <Link to={'/contacto'}><li className='liNavBar'>Contacto</li></Link>
--- src/components/Routes/Contacto/Contacto.jsx
+++ src/components/Routes/Contacto/Contacto.jsx
@@ -1,8 +1,34 @@
-
+import '../../../css/contacto.css'
 
 const Contacto = () => {
     
-    return <h1>Contacto</h1>
+    return(
+        <>
+            <form className='containerForm'>
+                <div class="form-group">
+                    <label>Nombre y Apellido</label>
+                    <input type="text" className="form-control" id="exampleInputName"/>
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" id="exampleInputEmail"/>
+                </div>
+                <div class="form-group">
+                    <label>Numero Celular</label>
+                    <input type="number" className="form-control" id="exampleInputNumber"/>
+                </div>
+                <div class="form-group">
+                    <label>Numero Fijo</label>
+                    <input type="number" className="form-control" id="exampleInputNumber"/>
+                </div>
+                <div style={{paddingBottom:'15px'}} class="form-group">
+                    <label>Mensaje</label>
+                    <input type="text" className="form-control" id="exampleInputMessage"/>
+                </div>
+                <button type="submit" style={{backgroundColor:'rgb(47, 177, 177)',borderColor:'rgb(47, 177, 177)'}} className="btn btn-primary">Enviar</button>
+            </form>
+        </>
+    )
 }
 
 export default Contacto;
